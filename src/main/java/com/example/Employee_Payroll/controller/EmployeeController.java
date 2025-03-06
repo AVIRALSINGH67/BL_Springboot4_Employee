7ba267aadb8b9746e17dd303f3f6d8ca82f7fbc3
AVIRALSINGH67 <aviral.raghuvansh1411@gmail.com>
2025-03-06 13:13:16 +0530

    UC2: Implemented CRUD Operation

---
+++ src/main/java/com/example/Employee_Payroll/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.example.Employee_Payroll.controller;
+
+import com.example.Employee_Payroll.model.Employee;
+import com.example.Employee_Payroll.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/employeepayrollservice")
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+    @GetMapping("/all")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+    @GetMapping("/get/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping("/create")
+    public Employee createEmployee(@Valid @RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+
+    @PutMapping("/update/{id}")
+    public Employee updateEmployee(@PathVariable int id, @Valid @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteEmployee(@PathVariable int id) {
+        employeeService.deleteEmployee(id);
+    }
+}
