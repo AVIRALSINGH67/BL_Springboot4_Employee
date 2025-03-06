5cc957ac3b6df0a75d5f3826fb29f57934681b08
AVIRALSINGH67 <aviral.raghuvansh1411@gmail.com>
2025-03-06 15:21:26 +0530

    UC4: Introduced Service Class To Project

--- src/main/java/com/example/Employee_Payroll/controller/EmployeeController.java
+++ src/main/java/com/example/Employee_Payroll/controller/EmployeeController.java
@@ -1,33 +1,39 @@
 package com.example.Employee_Payroll.controller;
 
+import com.example.Employee_Payroll.dto.EmployeeDTO;
 import com.example.Employee_Payroll.model.Employee;
 import com.example.Employee_Payroll.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeeController {
+
     @Autowired
     private EmployeeService employeeService;
+
     @GetMapping("/all")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
+
     @GetMapping("/get/{id}")
     public Employee getEmployeeById(@PathVariable int id) {
         return employeeService.getEmployeeById(id);
     }
 
     @PostMapping("/create")
-    public Employee createEmployee(@Valid @RequestBody Employee employee) {
-        return employeeService.addEmployee(employee);
+    public Employee createEmployee(@Valid @RequestBody EmployeeDTO employeeDTO) {
+        return employeeService.addEmployee(employeeDTO);
     }
 
     @PutMapping("/update/{id}")
-    public Employee updateEmployee(@PathVariable int id, @Valid @RequestBody Employee employee) {
-        return employeeService.updateEmployee(id, employee);
+    public Employee updateEmployee(@PathVariable int id, @Valid @RequestBody EmployeeDTO employeeDTO) {
+        return employeeService.updateEmployee(id, employeeDTO);
     }
 
     @DeleteMapping("/delete/{id}")
